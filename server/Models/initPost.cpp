bb52d07162809451110487039eb94cb8bb97e545
pukuba <skatmddnjs0604@gmail.com>
2020-12-23 21:10:10 +0900

    added Dummy Data Generator

--- server/Models/initPost.cpp
+++ server/Models/initPost.cpp
@@ -21,16 +21,56 @@ void sample2(ostream &fout)
     fout << "\"id\":" << id++ << ",";
     fout << "\"title\":\"Second Post!\",";
     fout << "\"content\":\"Pukuba's Second Post...! >.<! \"";
+    fout << "},";
+}
+char addedChar()
+{
+    int randomNumber = rand() % 61;
+    if (randomNumber < 10)
+        return randomNumber + '0';
+    else if (randomNumber < 36)
+    {
+        randomNumber -= 10;
+        return randomNumber + 'A';
+    }
+    randomNumber -= 35;
+    return randomNumber + 'a';
+}
+void writePost(ostream &fout)
+{
+    int titleLength = 10 + (rand() % 20);
+    int contentLength = 30 + (rand() % 100);
+    string title = "";
+    string content = "";
+    for (int i = 1; i <= titleLength; i++)
+    {
+        title += addedChar();
+    }
+    for (int i = 1; i <= contentLength; i++)
+    {
+        content += addedChar();
+    }
+    fout << "{";
+    fout << "\"id\":" << id++ << ",";
+    fout << "\"title\":\"" << title << "\",";
+    fout << "\"content\":\"" << content << "\"";
     fout << "}";
 }
 int main()
 {
     ofstream fout;
-    fout.open("post.json");
+    fout.open("post.db.json");
     fout << "[";
 
     sample1(fout);
     sample2(fout);
+
+    for (int i = 1; i <= 9997; i++)
+    {
+        writePost(fout);
+        fout << ",";
+    }
+    writePost(fout);
 
     fout << "]";
     return 0;

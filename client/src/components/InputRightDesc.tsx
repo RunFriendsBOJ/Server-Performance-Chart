b04077a5dbe953a87b5a4eaa606b96976b8039ce
WonDongGyu <windowdong11@naver.com>
2020-12-25 03:14:43 +0900

    입력란 오른쪽에 설명 있는 컴포넌트 작성

---
+++ client/src/components/InputRightDesc.tsx
@@ -0,0 +1,30 @@
+import { FormControl, InputGroup } from "react-bootstrap";
+import { onChangeFunc } from "../types/eventHandler";
+
+
+interface Props {
+    description: string
+
+    value?: string
+    onChange? : onChangeFunc
+
+    children?: React.ReactChild
+    placeholder?: string
+    size? : "sm" | "lg"
+}
+
+export default function InputRightDesc({ description, value, onChange, children, placeholder, size="sm"} : Props) {
+    return (
+        <InputGroup className="mb-3" size={size}>
+            <FormControl
+                placeholder={placeholder}
+                onChange={onChange}
+                value = {value}
+                children={children}
+            />
+            <InputGroup.Append>
+                <InputGroup.Text id="basic-addon2">{description}</InputGroup.Text>
+            </InputGroup.Append>
+        </InputGroup>
+    )
+}

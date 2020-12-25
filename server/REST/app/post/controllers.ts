0d0a3b5aca351ba6a5081b668382d0613df27a0b
pukuba <skatmddnjs0604@gmail.com>
2020-12-26 01:22:06 +0900

    added log

--- server/REST/app/post/controllers.ts
+++ server/REST/app/post/controllers.ts
@@ -3,6 +3,8 @@ import mongodb from '../../../config/connectDB'
 
 const allPost = async (req: Request, res: Response) => {
     const db = await mongodb().get()
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    console.log(`ip=${ip}  call=${process.env.REST_SERVER}/post/all`)
     const obj = {
         items: await db.collection('post').find({}).toArray()
     }
@@ -11,7 +13,9 @@ const allPost = async (req: Request, res: Response) => {
 
 const getPost = async (req: Request, res: Response) => {
     const db = await mongodb().get()
-    let item = await db.collection('post').findOne({ id: ~~req.params.id })
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    console.log(`ip=${ip}  call=${process.env.REST_SERVER}/post/${req.params.id}`)
+    const item = await db.collection('post').findOne({ id: ~~req.params.id })
     res.status(200).send(item)
 }
 

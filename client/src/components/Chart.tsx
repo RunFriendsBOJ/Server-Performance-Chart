ef9a1d4f740b6dd35866657501712588ead5f30e
windowdong11 <windowdong11@naver.com>
2020-12-27 19:45:04 +0900

    ChartData에서 datasets의 구조 interface로 분리

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -5,16 +5,19 @@ interface ChartProps {
     title: string
     data : ChartData
 }
+
 interface ChartData {
-    labels: string[],
-    datasets : Array<{
-        label: string
-        data: number[]
-        fill?: boolean
-        backgroundColor: string
-        borderColor: string
-        showLine?: boolean
-    }>
+    labels: string[], // x축을 나눌 라벨
+    datasets : ChartDataset[]
+}
+
+interface ChartDataset {
+    label: string // 차트 위에 표시되는 이름
+    data: number[] // 데이터, 순서대로 표시됨
+    fill?: boolean // 채움옵션
+    backgroundColor: string // 선 채울 색(The line fill color)
+    borderColor: string // 선 색(The line color)
+    showLine?: boolean // 그래프에 표시여부
 }
 
 const options = {

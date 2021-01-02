63427423b0d145286bb2dfadf2d68f4666aac7ac
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-03 04:27:59 +0900

    Refactoring

--- server/config/getArtillery.ts
+++ server/config/getArtillery.ts
@@ -10,12 +10,13 @@ const artillery = async (req: Request, res: Response, type: String) => {
     const db = await mongodb().get()
     const queryId = req.params.id
     const queryData = await db.collection(type).findOne({ queryId })
-    const address = req.body.address // 주소
-    const duration = req.body.duration // 시간
-    const arrivalRate = req.body.arrivalRate // 초당 리퀘스트
-    const clientCount = req.body.clientCount // 동시 접속수
+    const {
+        address, // 주소
+        duration, // 시간
+        arrivalRate, // 초당 리퀘스트
+        clientCount // 동시 접속수
+    } = req.body
     const query = `artillery quick --duration ${duration} --rate ${arrivalRate} -n ${clientCount} ${address} -o ${logPath}/Log/${type}.${queryId}.json`
-    // artillery quick --duration ${duration} --rate ${arrivalRate} -n ${clientCount} ${address} -o ${logPath}/Log/${type}.${queryId}.json
     if (queryData === null) {
         exec(query, (error, stdout, stderr) => {
             if (error || stderr) {

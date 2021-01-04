3b9b8bb27543e86946144e8049edf91601b592f8
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-04 23:52:53 +0900

    added cors

--- server/Main/server.ts
+++ server/Main/server.ts
@@ -2,11 +2,17 @@ import express from 'express'
 import dotenv from 'dotenv'
 import routes from './routes'
 import bodyParser from 'body-parser'
-
+import cors from 'cors'
 dotenv.config({ path: '../.env' })
 const port = process.env.MID_SERVER || 7777
 const app = express()
 
+const corsOptions = {
+    origin: '*',
+    method: "POST",
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('', routes)

e9903e75384e203d665a5645dc797e570ef16ad5
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-04 20:57:30 +0900

    get -> post

--- server/Main/app/ProtoBuf/router.ts
+++ server/Main/app/ProtoBuf/router.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 
 import { getQuery } from './controllers'
 const router = Router()
-router.get("/:id", getQuery)
+router.post("/:id", getQuery)
 
 export default router

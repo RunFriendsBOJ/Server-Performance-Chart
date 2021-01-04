ca66d30d6fecce34b004a95dc9e828fb520c890d
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-04 23:02:38 +0900

    added testcode

---
+++ server/TEST/main.test.ts
@@ -0,0 +1,60 @@
+import fetch from 'node-fetch'
+import assert from 'assert'
+const server = `http://localhost:7777`
+const restServer = `http://localhost:5000`
+
+describe(`Main Server Test`, () => {
+    it(`REST Artillery Test-1`, async () => {
+        const data = {
+            address: restServer + '/post/all',
+            duration: 2,
+            arrivalRate: 4,
+            clientCount: 4
+        }
+        const response = await fetch(`${server}/rest/rest-test-1`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        const json = await response.json()
+        assert.strictEqual(json.intermediate[0].requestsCompleted, 32)
+        assert.strictEqual(json.intermediate[0].latencies.length, 32)
+    }).timeout(50000)
+
+    it(`REST Artillery Test-2`, async () => {
+        const data = {
+            address: restServer + '/post/all',
+            duration: 5,
+            arrivalRate: 10,
+            clientCount: 10
+        }
+        const response = await fetch(`${server}/rest/rest-test-2`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        const json = await response.json()
+        assert.strictEqual(json.intermediate[0].requestsCompleted, 500)
+        assert.strictEqual(json.intermediate[0].latencies.length, 500)
+    }).timeout(50000)
+
+    it(`REST Artillery Test-3`, async () => {
+        const data = {
+            address: restServer + '/post/1',
+            duration: 5,
+            arrivalRate: 10,
+            clientCount: 10
+        }
+        const response = await fetch(`${server}/rest/rest-test-3`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        const json = await response.json()
+        assert.strictEqual(json.intermediate[0].requestsCompleted, 500)
+        assert.strictEqual(json.intermediate[0].latencies.length, 500)
+    }).timeout(50000)
+})
